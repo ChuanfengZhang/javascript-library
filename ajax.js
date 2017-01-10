e427502045012f03892116569693002f96884eef
Chuck <wfhtzcf@gmail.com>
2017-01-10 11:07:37 +0800

    错别字更正

--- ajax.js
+++ ajax.js
@@ -59,13 +59,13 @@
             // 因为get系不需要传send参数，所以设置为null
             userOptions.data = null;
         }
-        //2.普安段是否走缓存，如果不走缓存则在url后面加一个随机数来防止缓存
+        //2.判断是否走缓存，如果不走缓存则在url后面加一个随机数来防止缓存
         if (userOptions.cache === false) {
             // 因为search是有固定格式的key=value，如果只写一个value是不合法的，所以必须构造一个key，而且这个key不能和已有的key重复
             var random = "_=" + (Math.random() * 0xffffff).toFixed(0);
             userOptions.url = tool.hasSearch(userOptions.url, random);
         }
-        // ajax操作第二部
+        // ajax操作第二步
         xhr.open(userOptions.type, userOptions.url, userOptions.async);
 
         //2.1 设置自定义请求首部信息
@@ -96,25 +96,25 @@
                 // 低版本IE
                 setTimeout(function() {
                     if (xhr.readyState !== 4) {
-                        // 强制终止http事物
+                        // 强制终止http事务
                         xhr.abort();
                     }
                 }, xhr.timeout);
             }
         }
         //2.5 是否需要处理给服务器发送的数据，判断processData是否为true
-        // 当给服务器范松的数据为二进制或者formData的时候，不惜要处理这个数据
+        // 当给服务器发送的数据为二进制或者formData的时候，不需要处理这个数据
         // 要把processData设置为false
         if (/^(post|put)$/igm.test(userOptions.type) && userOptions.processData === true) {
             userOptions.data = tool.encodeToURIString(userOptions.data);
         }
         // ajax 第三步：接收响应
         xhr.onreadystatechange = function() {
-            // http的事物是否完成
+            // http的事务是否完成
             if (xhr.readyState === 4) {
                 // 获取响应主体
                 var responseText = xhr.responseText;
-                // 判断状态吗是否成功
+                // 判断状态码是否成功
                 if (/^2\d{2}$/.test(xhr.status)) {
                     // 判断是否需要把响应主体格式化为json对象
                     if (userOptions.dataType === "json") {
@@ -127,7 +127,7 @@
                         }
                     }
                     userOptions.success(responseText);
-                    // 如果响应吗是粗无的类型
+                    // 如果响应码是错误的类型
                 } else if (/^(4|5)\d{2}$/.test(xhr.status)) {
                     // 直接执行error
                     userOptions.error(xhr.status);
